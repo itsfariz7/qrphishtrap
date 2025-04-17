24b40bf01fd7865d87effbe339d7ed2b37ecae31
unknown <farizrizqullah6@gmail.com>
2025-04-17 22:46:34 +0700

    initial commit

--- app.py
+++ app.py
@@ -111,8 +111,9 @@ def analyze_qr():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000)
+
 
 
 @app.route('/')
