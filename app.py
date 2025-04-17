a1dde9e1f72d129c0b47e87c3a0b4bea16da47ac
itsfariz7 <farizrizqullah6@gmail.com>
2025-04-18 00:24:34 +0700

    Ensure all dependencies are installed

--- app.py
+++ app.py
@@ -102,4 +102,5 @@ def analyze_qr():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
